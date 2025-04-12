767aa34dfdc68526bafc83160294dca1495be81d
Tanmai <tanmaivinay0@gmail.com>
2025-04-12 22:56:11 +0530

    your message

--- 1.py
+++ 1.py
@@ -9,7 +9,8 @@ from tensorflow.keras.preprocessing import image
 import nltk
 from PIL import Image
 import io
-
+# First Streamlit command must be set_page_config
+st.set_page_config(page_title="SmartSpoon - Food Analyzer", layout="wide")
 # Download NLTK resources
 try:
     nltk.download('vader_lexicon', quiet=True)
